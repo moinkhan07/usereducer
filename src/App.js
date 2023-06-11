3f2a99a9dddd7f9e20bfe43dbf2ce973c063e643
Moin Khan <moin6k@gmail.com>
2023-06-11 11:30:50 +0530

    useReducer hook practice code added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useReducer } from 'react';
 import './App.css';
+import Header from "./components/Header"
+
+const reducer = (state,action)=>{
+  if(action.type == "INC"){
+    return state + 2;
+  }else if(action.type == "DEC"){
+    return state - 1;
+  }else{
+    return state * 2;
+  }
+}
 
 function App() {
+
+  const [state,dispatch]  =  useReducer(reducer,0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <div className='bd'>
+    <h1>{state}</h1>
+    <button onClick={()=> dispatch({type:"INC"})}>Increment By 2</button>
+    <button onClick={()=> dispatch({type:"DEC"})}>Decrement By 1</button>
+    <button onClick={()=> dispatch({type:"MUL"})}>Multiply By 2</button>
+    </div>
     </div>
   );
 }
